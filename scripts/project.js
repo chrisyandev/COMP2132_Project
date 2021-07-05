6197a63fcb34271c08fb2b116fc2318b830e86c2
chrisyandev <czyanhome@hotmail.com>
2021-07-04 20:40:46 -0700

    Hangman image updates, player can win or lose

--- scripts/project.js
+++ scripts/project.js
@@ -1,19 +1,29 @@
 // HTML elements
+const hangmanStateEl = document.getElementById("hangman-state");
 const lettersEl = document.getElementById("letters");
 const wordDisplayEl = document.getElementById("word-display");
+const closeWindowButtonEl = document.querySelector("#pop-up .close-window-button");
+const popUpEl = document.getElementById("pop-up");
+const dimmerEl = document.getElementById("dimmer");
+const gameEndMessageEl = document.getElementById("game-end-message");
+const letterButtons = [];
 
 // Constant values
 const firstLetterAscii = 65;
 const lastLetterAscii = 90;
+const showPopupAfter = 3000;
+const fadeInPopupTime = 40;
+const totalTries = 6;
 
-// Game variables
+// Game values
 let currentWord;
 let currentHint;
 let hangmanState = 0;
+let lettersCorrect = 0;
 let currentLetters = [];
 
 // Other variables
-let html = "";
+let showPopupHandler;
 
 const words = [
     {
@@ -67,39 +77,116 @@ class LetterButton {
     getButton() {
         return this.button;
     }
+
+    enable() {
+        this.button.disabled = false;
+    }
 }
 
 for (let i = firstLetterAscii; i <= lastLetterAscii; i++) {
     const letterButton = new LetterButton(String.fromCharCode(i));
+    letterButtons.push(letterButton);
     lettersEl.appendChild(letterButton.getButton());
-    letterButton.getButton().addEventListener("click", checkForLetter);
+    letterButton.getButton().addEventListener("click", function(evt) {
+        evt.currentTarget.disabled = true;
+        checkForLetter(evt.currentTarget.textContent);
+        checkGameState();
+    });
 }
 
-const randomWord = pickRandomWord();
-currentWord = randomWord.word.toUpperCase();
-currentHint = randomWord.hint;
-console.log(randomWord);
+initializeGame();
 
-wordDisplayEl.innerHTML = "";
-currentLetters = [];
-for (let i = 0; i < currentWord.length; i++) {
-    const letter = document.createElement("SPAN");
-    letter.className = "letter-input";
-    letter.textContent = "_";
-    currentLetters.push(letter);
-    wordDisplayEl.appendChild(letter);
-}
+function initializeGame() {
+    const randomWord = pickRandomWord();
+    currentWord = randomWord.word.toUpperCase();
+    currentHint = randomWord.hint;
+    hangmanState = 0;
+    lettersCorrect = 0;
+    currentLetters = [];
 
-function checkForLetter() {
-    const key = this.textContent.toUpperCase().charAt(0);
+    hangmanStateEl.src = `images/hangman-${hangmanState}.png`;
+    hangmanStateEl.alt = `hangman ${hangmanState}`;
+
+    wordDisplayEl.innerHTML = "";
+    currentLetters = [];
     for (let i = 0; i < currentWord.length; i++) {
-        if (key === currentWord.charAt(i)) {
-            currentLetters[i].textContent = key;
-        }
+        const letter = document.createElement("SPAN");
+        letter.className = "letter-input";
+        letter.textContent = "_";
+        currentLetters.push(letter);
+        wordDisplayEl.appendChild(letter);
     }
+
+    letterButtons.forEach(function(lb) {
+        lb.enable();
+    });
+
+    console.log(randomWord);
 }
 
 function pickRandomWord() {
     const randNum = Math.floor(Math.random() * words.length);
     return words[randNum];
 }
+
+function checkForLetter(letter) {
+    const key = letter.toUpperCase().charAt(0);
+    let letterFound = false;
+    for (let i = 0; i < currentWord.length; i++) {
+        if (key === currentWord.charAt(i)) {
+            letterFound = true;
+            currentLetters[i].textContent = key;
+            lettersCorrect++;
+        }
+    }
+    if (!letterFound) {
+        nextHangmanState();
+    }
+}
+
+function nextHangmanState() {
+    hangmanState++;
+    hangmanStateEl.src = `images/hangman-${hangmanState}.png`;
+    hangmanStateEl.alt = `hangman ${hangmanState}`;
+}
+
+function checkGameState() {
+    if (lettersCorrect >= currentWord.length) {
+        winGame();
+    } else if (hangmanState >= totalTries) {
+        loseGame();
+    }
+}
+
+function winGame() {
+    gameEndMessageEl.textContent = `You Won! Word was ${currentWord}`;
+    fadeIn(popUpEl, 1);
+    fadeIn(dimmerEl, 0.5);
+}
+
+function loseGame() {
+    gameEndMessageEl.textContent = `You Lost! Word was ${currentWord}`;
+    fadeIn(popUpEl, 1);
+    fadeIn(dimmerEl, 0.5);
+}
+
+function fadeIn(element, maxOpacity) {
+    element.style.display = "block";
+    const opacityHandler = setInterval(function() {
+        elementStyles = window.getComputedStyle(element);
+        const currentOpacity = +elementStyles.getPropertyValue("opacity");
+        if (currentOpacity < maxOpacity) {
+            element.style.opacity = `${currentOpacity + 0.05}`;
+        } else {
+            clearInterval(opacityHandler);
+        }
+    }, fadeInPopupTime);
+}
+
+closeWindowButtonEl.onclick = function() {
+    popUpEl.style.display = "none";
+    dimmerEl.style.display = "none";
+    popUpEl.style.opacity = "0.0";
+    dimmerEl.style.opacity = "0.0";
+    initializeGame();
+};
